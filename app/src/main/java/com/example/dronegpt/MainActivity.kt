dee4ae8d83060b33e512e2001da1f5cb4ae48734
mhconradt <mhconradt@protonmail.com>
2023-12-19 19:59:20 -0800

    Use OpenAI message format

--- app/src/main/java/com/example/dronegpt/MainActivity.kt
+++ app/src/main/java/com/example/dronegpt/MainActivity.kt
@@ -4,8 +4,6 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,21 +11,15 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.example.compose.jetchat.theme.DroneGPTTheme
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -35,6 +27,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
+import com.example.dronegpt.chat.ChatMessage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,7 +35,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             DroneGPTTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    Conversation(SampleData.conversationSample)
+                    ConversationHistory(SampleData.conversationSample)
                 }
             }
         }
@@ -52,34 +45,30 @@ class MainActivity : ComponentActivity() {
 data class Message(val author: String, val body: String)
 
 @Composable
-fun MessageCard(msg: Message) {
+fun MessageCard(msg: ChatMessage) {
     Row(modifier = Modifier.padding(all = 8.dp)) {
-        Image(
-            painter = painterResource(R.drawable.profile_picture),
-            contentDescription = "Contact profile picture",
-            modifier = Modifier
-                .size(40.dp)
-                .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
-        )
-
-        Spacer(modifier = Modifier.width(8.dp))
-
-        var isExpanded by remember { mutableStateOf(false)}
+        var isExpanded by remember { mutableStateOf(false) }
         val surfaceColor by animateColorAsState(
             if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
         )
 
         Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
-                text = msg.author,
+                text = msg.role,
                 color = MaterialTheme.colorScheme.secondary,
                 style = MaterialTheme.typography.titleSmall
             )
             Spacer(modifier = Modifier.height(4.dp))
-            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp, color = surfaceColor, modifier = Modifier.animateContentSize().padding(1.dp)) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
                 Text(
-                    text = msg.body,
+                    text = msg.content,
                     modifier = Modifier.padding(all = 4.dp),
                     maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.bodyMedium
@@ -97,87 +86,35 @@ fun MessageCard(msg: Message) {
 )
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(Message("Android", "Jetpack Compose"))
+    MessageCard(ChatMessage("assistant", "Hi, how can I help you today?"))
 }
 
 /**
  * SampleData for Jetpack Compose Tutorial
- */x
+ */
+
 object SampleData {
     // Sample conversation data
     val conversationSample = listOf(
-        Message(
-            "Lexi",
-            "Test...Test...Test..."
+        ChatMessage(
+            "user",
+            "Hello",
         ),
-        Message(
-            "Lexi",
-            """List of Android versions:
-            |Android KitKat (API 19)
-            |Android Lollipop (API 21)
-            |Android Marshmallow (API 23)
-            |Android Nougat (API 24)
-            |Android Oreo (API 26)
-            |Android Pie (API 28)
-            |Android 10 (API 29)
-            |Android 11 (API 30)
-            |Android 12 (API 31)""".trim()
+        ChatMessage(
+            "assistant",
+            "Hi, how can I help you today?"
         ),
-        Message(
-            "Lexi",
-            """I think Kotlin is my favorite programming language.
-            |It's so much fun!""".trim()
-        ),
-        Message(
-            "Lexi",
-            "Searching for alternatives to XML layouts..."
-        ),
-        Message(
-            "Lexi",
-            """Hey, take a look at Jetpack Compose, it's great!
-            |It's the Android's modern toolkit for building native UI.
-            |It simplifies and accelerates UI development on Android.
-            |Less code, powerful tools, and intuitive Kotlin APIs :)""".trim()
-        ),
-        Message(
-            "Lexi",
-            "It's available from API 21+ :)"
-        ),
-        Message(
-            "Lexi",
-            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
-        ),
-        Message(
-            "Lexi",
-            "Android Studio next version's name is Arctic Fox"
-        ),
-        Message(
-            "Lexi",
-            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
-        ),
-        Message(
-            "Lexi",
-            "I didn't know you can now run the emulator directly from Android Studio"
-        ),
-        Message(
-            "Lexi",
-            "Compose Previews are great to check quickly how a composable layout looks like"
-        ),
-        Message(
-            "Lexi",
-            "Previews are also interactive after enabling the experimental setting"
-        ),
-        Message(
-            "Lexi",
-            "Have you tried writing build.gradle with KTS?"
-        ),
+        ChatMessage(
+            "user",
+            "I want to fly my drone"
+        )
     )
 }
 
 @Composable
-fun Conversation(messages: List<Message>) {
+fun ConversationHistory(messages: List<ChatMessage>) {
     LazyColumn {
-        items(messages) { message -> MessageCard(msg = message)}
+        items(messages) { message -> MessageCard(msg = message) }
     }
 }
 
@@ -185,6 +122,6 @@ fun Conversation(messages: List<Message>) {
 @Composable
 fun PreviewConversation() {
     DroneGPTTheme {
-        Conversation(messages = SampleData.conversationSample)
+        ConversationHistory(messages = SampleData.conversationSample)
     }
 }
